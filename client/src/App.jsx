a99372c57f2d6d94e769abe706b692eacd48de1d
ANZAR <anzarkhan790@gmail.com>
2024-02-18 01:22:06 +0530

    Created Navbar and Menu

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,11 @@ import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
 import About from "./pages/About";
+import Header from "./components/Header";
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/about" element={<About />}></Route>
