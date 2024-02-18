483faf60b6a55c364ad5e629635dabc2075950e6
ANZAR <anzarkhan790@gmail.com>
2024-02-18 22:35:22 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+mongoose.connect(process.env.DB_URI).then(()=>{
+        console.log('MONGODB is connected');
+    }).catch(e=>{
+        console.log(e);
+    })
+
+
 const app=express();
 app.listen(3000,()=>{
     console.log('Server is running at http://localhost:3000');
