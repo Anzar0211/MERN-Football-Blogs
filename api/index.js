33ba11a1a83ddc6fa98c354c80c31964abd79454
ANZAR <anzarkhan790@gmail.com>
2024-02-19 00:08:11 +0530

    signup api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/userRoutes.js';
+import authRoutes from './routes/authRoutes.js';
 dotenv.config();
 mongoose.connect(process.env.DB_URI).then(()=>{
         console.log('MONGODB is connected');
@@ -10,6 +12,9 @@ mongoose.connect(process.env.DB_URI).then(()=>{
 
 
 const app=express();
+app.use(express.json());
 app.listen(3000,()=>{
     console.log('Server is running at http://localhost:3000');
 })
+app.use('/api/user',userRoutes);
+app.use('/api/auth',authRoutes);
