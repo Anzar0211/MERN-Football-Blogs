63b2e096fc4b0449185965ddda489f7590dfc6f0
ANZAR <anzarkhan790@gmail.com>
2024-02-20 12:13:36 +0530

    signin api route

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,6 +1,7 @@
 import User  from '../models/user.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 export const signup=async(req,res,next)=>{
     const{username,email,password}=req.body;
     if(!username || !email || !password || username==='' || email==='' || password===''){
@@ -17,4 +18,32 @@ export const signup=async(req,res,next)=>{
         
     }
     
+}
+export const signin=async(req,res,next)=>{
+    const{email,password}=req.body;
+    if(!email  || !password || email==='' || password===''){
+        next(errorHandler(400,'All Fields are Required'))
+    }
+    try{
+        const validUser=await User.findOne({email});
+        if(!validUser){
+            return next(errorHandler(404,"Invalid Email or Password"));
+        }
+        const validPassword = bcryptjs.compareSync(password,validUser.password);
+        if(!validPassword){
+            return next(errorHandler(401,"Invalid Email or Password"));
+        }
+        const token=jwt.sign(
+            {id:validUser._id},
+            process.env.JWT_SECRET,
+        )
+        const{password:pass,...rest}=validUser._doc;
+        res.status(200).cookie('access_token',token,{
+            httpOnly:true
+        }).json(rest);
+        
+    }catch(e){
+        next(e);
+    }
+
 }
