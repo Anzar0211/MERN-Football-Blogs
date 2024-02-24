02e6e51fca8c0478755843b273e04977c8a3bf31
ANZAR <anzarkhan790@gmail.com>
2024-02-24 09:58:14 +0530

    added admin functionality

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -32,7 +32,7 @@ export const signin=async(req,res,next)=>{
             return next(errorHandler(401,"Invalid Email or Password"));
         }
         const token=jwt.sign(
-            {id:validUser._id},
+            {id:validUser._id,isAdmin:validUser.isAdmin},
             process.env.JWT_SECRET,
             {expiresIn:'2d'}
         )
@@ -54,7 +54,7 @@ export const google = async (req, res, next) => {
     const user = await User.findOne({ email });
     if (user) {
       const token = jwt.sign(
-        { id: user._id},
+        { id: user._id,isAdmin:user.isAdmin},
         process.env.JWT_SECRET
       );
       
@@ -84,7 +84,7 @@ export const google = async (req, res, next) => {
       });
       await newUser.save();
       const token = jwt.sign(
-        { id: newUser._id},
+        { id: newUser._id, isAdmin : newUser.isAdmin },
         process.env.JWT_SECRET
       );
       const { password, ...rest } = newUser._doc;
